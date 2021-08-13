05b83de3302b9680783bce6fee3f350d899be097
omgnuts <peanuts@munchpress.com>
2021-08-13 23:09:58 +0800

    adding synchronizer to camera.dart

--- packages/camera/camera/lib/camera.dart
+++ packages/camera/camera/lib/camera.dart
@@ -5,6 +5,7 @@
 export 'src/camera_controller.dart';
 export 'src/camera_image.dart';
 export 'src/camera_preview.dart';
+export 'src/open_synchronizer.dart';
 
 export 'package:camera_platform_interface/camera_platform_interface.dart'
     show
